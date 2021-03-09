faf07442503b2c754de44acd2fd2358117b40244
YichengShen <jsszsyc@hotmail.com>
2021-03-09 08:35:33 -0500

    add json

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ const _ = require('lodash');
 // html
 import './index.html'
 
+// json
+import data from './data.json';
+
 // js modules
 import renderIconHeader from './components/icon_header/icon_header.js'
 import renderDragDropArea, {addDragDrop} from './components/drag_drop/drag_drop.js'
 
-renderMainPage()
+renderMainPage(data);
 
-function renderMainPage() {
+function renderMainPage(data) {
     document.body.innerHTML = `
 
         ${renderIconHeader()}
